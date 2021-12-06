427fe6563dccabf6826af3d9ee3e6c9ef37da3ae
Rajesh Prasad Sah <94773900+Rajeshhsah@users.noreply.github.com>
2021-12-06 12:06:45 +0545

    Update Lab8.py

--- Day 3/Lab8.py
+++ Day 3/Lab8.py
@@ -3,15 +3,17 @@
 #for consecutive 3 times and if the limit exceeds than print "Attempt finished".
 
 
-u=str(input("Enter Your Username :"))
-p=str(input("Enter Your Password :"))
-#checking first time 
-u1=str(input("Enter Your Username Again :"))
-p1=str(input("Enter Your Password Again :"))
-
-if (u==u1) and (p==p1):
-    print("Login Successfully")
-elif
-u2=str(input("Enter Your Username Again :"))
-p3=str(input("Enter Your Password Again :"))
-print()
+username=input("Enter Your Username: ")
+password=input("Enter your Password: ")
+for i in range(0,3):
+    print("Enter Your Credentials")
+    username1=input("Enter Your Username: ")
+    password1=input("Enter your Password: ")
+    if (username==username1) and (password==password1):
+        print("You are successfully Logged.")
+        break
+    else:
+        if(username!=username1 and password==password1):
+            print("Invalid Credentials")
+else:
+    print("3 Attempt Finished")
