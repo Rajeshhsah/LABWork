67e611580f27175f8b8612bfda530b09a6975b84
Rajesh Prasad Sah <94773900+Rajeshhsah@users.noreply.github.com>
2021-12-12 09:08:04 +0545

    extra folder

--- Other Python projects/a (3).py
+++
@@ -1,8 +0,0 @@
-#convert seconds
-S = int (input("enter the value for second:"))
-Day = (((S/60) /60) /24)
-print(f"total day for given seconds: {Day}")
-Hour = ((S/60)/60)
-print (f"total hous for given seconds:{Hour}")
-Minute = (S /60)
-print (f"total minutes for given second:s{Minute}")
